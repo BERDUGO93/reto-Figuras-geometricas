67dfca8a67e2501a31404385deafafa140ae9385
Diego Berdugo <104031707+BERDUGO93@users.noreply.github.com>
2023-05-09 10:38:39 -0500

    Update Pentagono.java

--- figurasg/figurasg/src/main/java/Pentagono.java
+++ figurasg/figurasg/src/main/java/Pentagono.java
@@ -1,96 +1,100 @@
-
-
 import java.util.Scanner;
 
-
 public class Pentagono extends figurasGeometricas {
-    private Double lado;
-  
-    private Double apotema;
-    private Double area;
-    private Double perimetro;
-    private Double Plado;
-    
-    
-    
-    public Pentagono() {
-    }
 
-    
+  private Double lado;
 
-    public Pentagono(Double lado, Double apotema, Double area, Double perimetro, Double plado) {
-        this.lado = lado;
-        this.apotema = apotema;
-        this.area = area;
-        this.perimetro = perimetro;
-        Plado = plado;
-    }
+  private Double apotema;
+  private Double area;
+  private Double perimetro;
+  private Double Plado;
 
+  public Pentagono() {}
 
+  public Pentagono(
+    Double lado,
+    Double apotema,
+    Double area,
+    Double perimetro,
+    Double plado
+  ) {
+    this.lado = lado;
+    this.apotema = apotema;
+    this.area = area;
+    this.perimetro = perimetro;
+    Plado = plado;
+  }
 
-    public Double getLado() {
-        return lado;
-    }
+  public Double getLado() {
+    return lado;
+  }
 
-    public void setLado(Double lado) {
-        this.lado = lado;
-    }
+  public void setLado(Double lado) {
+    this.lado = lado;
+  }
 
-    public Double getApotema() {
-        return apotema;
-    }
+  public Double getApotema() {
+    return apotema;
+  }
 
-    public void setApotema(Double apotema) {
-        this.apotema = apotema;
-    }
+  public void setApotema(Double apotema) {
+    this.apotema = apotema;
+  }
 
-    public Double getArea() {
-        return area;
-    }
+  public Double getArea() {
+    return area;
+  }
 
-    public void setArea(Double area) {
-        this.area = area;
-    }
+  public void setArea(Double area) {
+    this.area = area;
+  }
 
-    public Double getPerimetro() {
-        return perimetro;
-    }
+  public Double getPerimetro() {
+    return perimetro;
+  }
 
-    public void setPerimetro(Double perimetro) {
-        this.perimetro = perimetro;
-    }
+  public void setPerimetro(Double perimetro) {
+    this.perimetro = perimetro;
+  }
 
-    public Double getPlado() {
-        return Plado;
-    }
+  public Double getPlado() {
+    return Plado;
+  }
 
-    public void setPlado(Double plado) {
-        Plado = plado;
-    }
+  public void setPlado(Double plado) {
+    Plado = plado;
+  }
 
-    Scanner entrada = new Scanner(System.in);
+  Scanner entrada = new Scanner(System.in);
 
+  @Override
+  public void calcularArea() {
+    area = ((lado * 5) * apotema) / 2;
 
+    System.out.println(" el area del Pentagono es " + area);
+  }
 
-    @Override
-    public void calcularArea() {
+  @Override
+  public void calcularPerimetro() {
+    perimetro = 5 * Plado;
 
-        area=((lado*5)*apotema)/2;
+    System.out.println("el perimetro del pentagono es  " + perimetro);
+  }
 
-        System.out.println(" el area del Pentagono es "+area);
+  @Override
+  public void ingresarDatosPerimetro() {
+    System.out.println(
+      "ingrese la medida de un lado del Pentagono para calcular el perimetro"
+    );
+    pent.setPlado(entrada.nextDouble());
+  }
 
-       
-    }
+  @Override
+  public void ingresarDatosArea() {
+    System.out.println("ingrese la lado del pentagono");
+    pent.setLado(entrada.nextDouble());
 
-    @Override
-    public void calcularPerimetro() {
-        perimetro=5*Plado;
-
-        System.out.println("el perimetro del pentagono es  "+perimetro);
-        
-    }
-
-
-    
+    System.out.println("ingrese el apotema del pentagono");
+    pent.setApotema(entrada.nextDouble());
+  }
 }
-
