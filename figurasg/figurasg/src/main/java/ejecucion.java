33bbfff83726d96d925642522a1461b7f431ceb7
Diego Berdugo <104031707+BERDUGO93@users.noreply.github.com>
2023-05-09 11:10:15 -0500

    Update ejecucion.java

--- figurasg/figurasg/src/main/java/ejecucion.java
+++ figurasg/figurasg/src/main/java/ejecucion.java
@@ -9,34 +9,64 @@ public class ejecucion {
 
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
+    int select = mostrarMenu(entrada);
+    manejarOpcionSeleccionada(select, entrada);
+  }
+
+  private static int mostrarMenu(Scanner entrada) {
     System.out.println(
       "Seleccione una figura geométrica para calcular su área y perímetro:\n1: Triángulo\n2: Rombo\n3: Pentágono"
     );
-    int select = entrada.nextInt();
-    System.out.println("que desea calcular  : \n1:Area \n2:perimetro ");
-    opcion = entrada.nextInt();
+    return entrada.nextInt();
+  }
+
+  private static void manejarOpcionSeleccionada(int select, Scanner entrada) {
     switch (select) {
       case 1:
-        if (opcion == 1) {
-          triangulo.calcularArea();
-        } else if (opcion == 2) {
-          triangulo.calcularPerimetro();
-        }
+        manejarTriangulo(entrada);
         break;
       case 2:
-        if (opcion == 1) {
-          rombo.calcularArea();
-        } else if (opcion == 2) {
-          rombo.calcularPerimetro();
-        }
+        manejarRombo(entrada);
         break;
       case 3:
-        if (opcion == 1) {
-          pent.calcularArea();
-        } else if (opcion == 2) {
-          pent.calcularPerimetro();
-        }
+        manejarPentagono(entrada);
         break;
+      default:
+        System.out.println("Opción inválida");
+        break;
+    }
+  }
+
+  private static void manejarTriangulo(Scanner entrada) {
+    System.out.println("¿Qué desea calcular?\n1: Área\n2: Perímetro");
+    int opcion = entrada.nextInt();
+    if (opcion == 1) {
+      triangulo.calcularArea();
+    } else if (opcion == 2) {
+      triangulo.calcularPerimetro();
+    }
+  }
+
+  private static void manejarRombo(Scanner entrada) {
+    System.out.println("¿Qué desea calcular?\n1: Área\n2: Perímetro");
+    int opcion = entrada.nextInt();
+    if (opcion == 1) {
+      rombo.ingresarDatosArea();
+      rombo.calcularArea();
+    } else if (opcion == 2) {
+      rombo.ingresarDatosPerimetro();
+      rombo.calcularPerimetro();
+    }
+  }
+
+  private static void manejarPentagono(Scanner entrada) {
+    System.out.println("¿Qué desea calcular?\n1: Área\n2: Perímetro");
+    int opcion = entrada.nextInt();
+    if (opcion == 1) {
+      pent.calcularArea();
+    } else if (opcion == 2) {
+      pent.ingresarDatosPerimetro();
+      pent.calcularPerimetro();
     }
   }
 }
