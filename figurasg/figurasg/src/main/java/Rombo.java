1515b983e102514614e52a343def94e374f50336
Diego Berdugo <104031707+BERDUGO93@users.noreply.github.com>
2023-05-09 10:39:21 -0500

    Update Rombo.java

--- figurasg/figurasg/src/main/java/Rombo.java
+++ figurasg/figurasg/src/main/java/Rombo.java
@@ -1,86 +1,99 @@
-
 import java.util.Scanner;
 
-public class Rombo extends figurasGeometricas{
-    private Double diagmenor;
-      private Double Rlado;
-       private Double diagmayor;
-    private Double area;
-    private Double perimetro;
+public class Rombo extends figurasGeometricas {
 
-    public Double getDiagmenor() {
-        return diagmenor;
-    }
+  private Double diagmenor;
+  private Double Rlado;
+  private Double diagmayor;
+  private Double area;
+  private Double perimetro;
 
-    public void setDiagmenor(Double diagmenor) {
-        this.diagmenor = diagmenor;
-    }
+  public Double getDiagmenor() {
+    return diagmenor;
+  }
 
-    public Double getRlado() {
-        return Rlado;
-    }
+  public void setDiagmenor(Double diagmenor) {
+    this.diagmenor = diagmenor;
+  }
 
-    public void setRlado(Double rlado) {
-        Rlado = rlado;
-    }
+  public Double getRlado() {
+    return Rlado;
+  }
 
-    public Double getDiagmayor() {
-        return diagmayor;
-    }
+  public void setRlado(Double rlado) {
+    Rlado = rlado;
+  }
 
-    public void setDiagmayor(Double diagmayor) {
-        this.diagmayor = diagmayor;
-    }
+  public Double getDiagmayor() {
+    return diagmayor;
+  }
 
-    public Double getArea() {
-        return area;
-    }
+  public void setDiagmayor(Double diagmayor) {
+    this.diagmayor = diagmayor;
+  }
 
-    public void setArea(Double area) {
-        this.area = area;
-    }
+  public Double getArea() {
+    return area;
+  }
 
-    public Double getPerimetro() {
-        return perimetro;
-    }
+  public void setArea(Double area) {
+    this.area = area;
+  }
 
-    public void setPerimetro(Double perimetro) {
-        this.perimetro = perimetro;
-    }
+  public Double getPerimetro() {
+    return perimetro;
+  }
 
-    public Rombo(){}
-    
-    
-    public Rombo(Double diagmenor, Double rlado, Double diagmayor, Double area, Double perimetro) {
-        this.diagmenor = diagmenor;
-        Rlado = rlado;
-        this.diagmayor = diagmayor;
-        this.area = area;
-        this.perimetro = perimetro;
-    }
+  public void setPerimetro(Double perimetro) {
+    this.perimetro = perimetro;
+  }
 
+  public Rombo() {}
 
-    Scanner entrada = new Scanner(System.in);
+  public Rombo(
+    Double diagmenor,
+    Double rlado,
+    Double diagmayor,
+    Double area,
+    Double perimetro
+  ) {
+    this.diagmenor = diagmenor;
+    Rlado = rlado;
+    this.diagmayor = diagmayor;
+    this.area = area;
+    this.perimetro = perimetro;
+  }
 
+  Scanner entrada = new Scanner(System.in);
 
+  @Override
+  public void calcularArea() {
+    area = (diagmayor * diagmenor) / 2;
 
-    
+    System.out.println(" el area del rombo es " + area);
+  }
 
-    @Override
-    public void calcularArea() {
-        area=(diagmayor*diagmenor)/2;
+  @Override
+  public void calcularPerimetro() {
+    perimetro = 4 * Rlado;
 
-        System.out.println(" el area del rombo es "+area);
-      
-    }
+    System.out.println("el perimetro del Rombo es  " + perimetro);
+  }
 
-    @Override
-    public void calcularPerimetro() {
-        perimetro=4*Rlado;
+  @Override
+  public void ingresarDatosPerimetro() {
+    System.out.println(
+      "ingrese la medida de un lado del Rombo para calcular el perimetro"
+    );
+    rombo.setRlado(entrada.nextDouble());
+  }
 
-        System.out.println("el perimetro del Rombo es  "+perimetro);
-    
-    }
+  @Override
+  public void ingresarDatosArea() {
+    System.out.println("ingrese la diagonal mayor del rombo");
+    rombo.setDiagmayor(entrada.nextDouble());
 
-
+    System.out.println("ingrese la diagonal menor del rombo");
+    rombo.setDiagmenor(entrada.nextDouble());
+  }
 }
