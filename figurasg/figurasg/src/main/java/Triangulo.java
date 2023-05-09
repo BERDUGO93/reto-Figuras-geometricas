ebdfb8ac902cccfc23882ac0eeb11af27c4161c0
Diego Berdugo <104031707+BERDUGO93@users.noreply.github.com>
2023-05-09 10:39:57 -0500

    Update Triangulo.java

--- figurasg/figurasg/src/main/java/Triangulo.java
+++ figurasg/figurasg/src/main/java/Triangulo.java
@@ -1,113 +1,93 @@
-
-
-
-
 public class Triangulo extends figurasGeometricas {
 
-   
-   
+  private Double lado;
+  private Double Base;
+  private Double Altura;
+  private Double Area;
+  private Double Perimetro;
 
-    private Double lado;
-    private Double Base;
-    private Double Altura;
-    private Double Area;
-    private Double Perimetro;
-    public Double getLado() {
-        return lado;
-    }
+  public Double getLado() {
+    return lado;
+  }
 
+  public void setLado(Double lado) {
+    this.lado = lado;
+  }
 
+  public Double getBase() {
+    return Base;
+  }
 
-    public void setLado(Double lado) {
-        this.lado = lado;
-    }
+  public void setBase(Double base) {
+    Base = base;
+  }
 
+  public Double getAltura() {
+    return Altura;
+  }
 
+  public void setAltura(Double altura) {
+    Altura = altura;
+  }
 
-    public Double getBase() {
-        return Base;
-    }
+  public Double getArea() {
+    return Area;
+  }
 
+  public void setArea(Double area) {
+    Area = area;
+  }
 
+  public Double getPerimetro() {
+    return Perimetro;
+  }
 
-    public void setBase(Double base) {
-        Base = base;
-    }
+  public void setPerimetro(Double perimetro) {
+    Perimetro = perimetro;
+  }
 
+  public Triangulo() {}
 
+  public Triangulo(
+    Double lado,
+    Double base,
+    Double altura,
+    Double area,
+    Double perimetro
+  ) {
+    this.lado = lado;
+    Base = base;
+    Altura = altura;
+    Area = area;
+    Perimetro = perimetro;
+  }
 
-    public Double getAltura() {
-        return Altura;
-    }
+  @Override
+  public void calcularArea() {
+    Area = (Base * Altura) / 2;
+    System.out.println(" el area del triangulo es " + Area);
+  }
 
+  @Override
+  public void calcularPerimetro() {
+    Perimetro = (lado * 3);
 
+    System.out.println("el perimetro del triangulo es " + Perimetro);
+  }
 
-    public void setAltura(Double altura) {
-        Altura = altura;
-    }
+  @Override
+  public void ingresarDatosPerimetro() {
+    System.out.println(
+      "ingrese la medida de un lado del triangulo para calcular el perimetro"
+    );
+    triangulo.setLado(entrada.nextDouble());
+  }
 
-
-
-    public Double getArea() {
-        return Area;
-    }
-
-
-
-    public void setArea(Double area) {
-        Area = area;
-    }
-
-
-
-    public Double getPerimetro() {
-        return Perimetro;
-    }
-
-
-
-    public void setPerimetro(Double perimetro) {
-        Perimetro = perimetro;
-    }
-
-
-
-    public Triangulo(){}
-   
-
-
-    public Triangulo(Double lado, Double base, Double altura, Double area, Double perimetro) {
-        this.lado = lado;
-        Base = base;
-        Altura = altura;
-        Area = area;
-        Perimetro = perimetro;
-    }
-
-
-
-
-
-    @Override
-    public void calcularArea() {
-        
-        Area=(Base*Altura)/2;
-
-        System.out.println(" el area del triangulo es "+Area);
-        
-    }
-
-
-
-    @Override
-    public void calcularPerimetro() {
-        Perimetro =(lado*3);
-
-        System.out.println("el perimetro del triangulo es " +Perimetro);
-        
-    }
-
-    }
-
-
-     
+  @Override
+  public void ingresarDatosArea() {
+    System.out.println("ingrese la base del triangulo");
+    triangulo.setBase(entrada.nextDouble());
+    System.out.println("ingrese el altura del triangulo");
+    triangulo.setAltura(entrada.nextDouble());
+  }
+}
